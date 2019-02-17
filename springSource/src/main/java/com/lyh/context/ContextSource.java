19c3a7047c977448cc5f1f70b3abee359daef8f6
lvyanghui <312386021@qq.com>
2019-02-17 14:29:45 +0800

    context源码学习

--- springSource/src/main/java/com/lyh/context/ContextSource.java
+++ springSource/src/main/java/com/lyh/context/ContextSource.java
@@ -1,5 +1,6 @@
 package com.lyh.context;
 
+import com.lyh.samples.ABean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,9 +9,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 2019/2/17 13:47
  */
 public class ContextSource {
+
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("");
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ABean aBean = (ABean) context.getBean("aBean");
+        aBean.methodA();
     }
 }
---
+++ springSource/src/main/java/com/lyh/samples/ABean.java
@@ -0,0 +1,21 @@
+package com.lyh.samples;
+
+/**
+ * Created by lvyanghui
+ * 2019/1/16 22:16
+ */
+public class ABean {
+    public void initMethod(){
+
+        System.out.println("ABean 执行初始化方法.....");
+    }
+
+    public void methodA(){
+        System.out.println("ABean 执行methodA.......");
+    }
+
+    public void destoryA(){
+        System.out.println("ABean 执行destoryA.......");
+    }
+
+}
