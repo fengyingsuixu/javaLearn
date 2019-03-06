7a86f1eb3b944e3256a2709079357c91a3a16350
lvyanghui <312386021@qq.com>
2019-03-06 22:16:42 +0800

    添加config配置

---
+++ configClient/src/main/java/com/lv/JarApp.java
@@ -0,0 +1,16 @@
+package com.lv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lvyanghui
+ * 2019/3/6 22:07
+ */
+@SpringBootApplication
+public class JarApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarApp.class,args);
+    }
+}
---
+++ configClient/src/main/java/com/lv/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.lv.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lvyanghui
+ * 2019/3/6 22:12
+ */
+
+@RestController
+public class UserController {
+
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private String age;
+    @Value("${address}")
+    private String address;
+
+    @RequestMapping("/user")
+    private String getUser(){
+        return "name:" + name + "age:" + age + "address:" + address;
+    }
+}
