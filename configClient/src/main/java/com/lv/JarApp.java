69aa4770e53fd1c1b2118659868874dfd7c2d997
lvyanghui <312386021@qq.com>
2019-03-06 22:28:24 +0800

    添加高可用配置

--- configClient/src/main/java/com/lv/JarApp.java
+++ configClient/src/main/java/com/lv/JarApp.java
@@ -2,12 +2,14 @@ package com.lv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by lvyanghui
  * 2019/3/6 22:07
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JarApp {
 
     public static void main(String[] args) {
