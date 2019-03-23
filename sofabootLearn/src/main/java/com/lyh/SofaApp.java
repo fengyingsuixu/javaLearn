943fa522cd5369325adbaa776069819f5cdd66e3
lvyanghui <312386021@qq.com>
2019-03-23 15:46:10 +0800

    添加sofaboot

---
+++ sofabootLearn/src/main/java/com/lyh/SofaApp.java
@@ -0,0 +1,16 @@
+package com.lyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lvyanghui
+ * 2019/3/23 15:30
+ */
+@SpringBootApplication
+public class SofaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaApp.class,args);
+    }
+}
